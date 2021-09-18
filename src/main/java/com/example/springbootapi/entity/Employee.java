9a12297b110cb6b08bb70ada8bfeb426a7340151
Vuong <vuong@11.com>
2021-09-18 10:30:21 +0700

    validation

--- src/main/java/com/example/springbootapi/entity/Employee.java
+++ src/main/java/com/example/springbootapi/entity/Employee.java
@@ -1,25 +1,41 @@
 package com.example.springbootapi.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
+@Builder
 @Table(name = "tbl_employeeapi")
 
 public class Employee {
     @Id
     @GeneratedValue
     private Long EmployeeID;
+
+    @NotBlank(message = "FirstName is mandatory")
+    @Length(min = 1,max = 15,message = "less than 15 and greater than one")
     private String firstName;
+
+    @NotBlank(message = "LastName is mandatory")
+    @Length(min = 1,max = 15,message = "less than 15 and greater than one")
     private String lastName;
+
+    @Email(message = "Please Input Email")
     private String email;
+
+    @NotBlank(message = "Phone is mandatory")
+    @Length(min = 9,max = 10,message = "phone min 9 and max 10")
+    private String phone;
 }
